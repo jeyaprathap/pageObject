6ab202b1db9cb736913b6ee89568332a26a1baa5
DESKTOP-U99CFGD\prath <sjeyaprathap@yahoo.com>
2018-07-24 23:49:00 +1000

    data file path updated

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -20,7 +20,7 @@ public class TestUtil extends TestBase{
 	public static long PAGE_LOAD_TIMEOUT = 20;
 	public static long IMPLICIT_WAIT = 20;
 	
-	public static String TESTDATA_SHEET_PATH = "C:/Users/prath/workspace/FreeCRMTest/src/main/java/com/crm/qa/testdata/FreeCRMTestData.xlsx";
+	public static String TESTDATA_SHEET_PATH = "D:/GitRepo/PageObjectModel/PageObjectModelMaster/src/main/java/com/crm/qa/testdata/FreeCrmTestData.xlsx";
 	
 	static Workbook book;
 	static Sheet sheet;
